3328eb1234b64ba318dbd5fa1b719ee171c2765e
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2024-03-25 20:45:51 +0000

    Database configs updated

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -35,7 +35,7 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
 
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://tradejournal:pa55word@localhost/tradejournal/?sslmode=disable", "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("TRADEJOURNAL_DB_DSN"), "PostgreSQL DSN")
 	flag.Parse()
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
